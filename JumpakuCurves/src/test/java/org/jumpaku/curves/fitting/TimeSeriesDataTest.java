669a5d51c23a11008961ec13e26efa53b860057c
Jumpaku <jumpaku1089@gmail.com>
2016-12-15 00:32:08 +0900

    tested TimeSeriesData

---
+++ JumpakuCurves/src/test/java/org/jumpaku/curves/fitting/TimeSeriesDataTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jumpaku.curves.fitting;
+
+import org.jumpaku.curves.fitting.TimeSeriesData;
+import org.apache.commons.math3.util.Precision;
+import org.jumpaku.curves.vector.Point;
+import org.jumpaku.curves.vector.Point3D;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tomohiko
+ */
+public class TimeSeriesDataTest {
+    
+    /**
+     * Test of getData method, of class TimeSeriesData.
+     */
+    @Test
+    public void testGetData() {
+        System.out.println("getPoint");
+        TimeSeriesData<Point3D> instance = new TimeSeriesData<>(new Point3D(1.0, -4.32, 0.00041), -0.38);
+        assertTrue(Point.equals(new Point3D(1.0, -4.32, 0.00041), instance.getData(), 1.0e-10));
+    }
+
+    /**
+     * Test of getTime method, of class TimeSeriesData.
+     */
+    @Test
+    public void testGetTime() {
+        System.out.println("getTime");
+        TimeSeriesData<Point3D> instance = new TimeSeriesData<>(new Point3D(1.0, -4.32, 0.00041), -0.38);
+        assertTrue(Precision.equals(-0.38, instance.getTime(), 1.0e-10));
+    }
+    
+}
